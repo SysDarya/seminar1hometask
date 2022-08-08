b44e2da35d2985177c37686e84ee78b5b7ca17c6
Sys Darya <sysdaryaandreevna@gmail.com>
2022-08-08 13:23:02 +0300

    006 task done

---
+++ 006/006.cs
@@ -0,0 +1,4 @@
+﻿// Показать последнюю цифру трёхзначного числа
+Console.WriteLine ("Введите трёхзначное число: ");
+int a = int.Parse (Console.ReadLine()!);
+Console.WriteLine ("Последняя цифра числа: " + a%10);
