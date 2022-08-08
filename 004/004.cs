933debac7a62ef06a693a19c197012b5b592b97d
Sys Darya <sysdaryaandreevna@gmail.com>
2022-08-08 13:00:38 +0300

    004 task done

---
+++ 004/004.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить является ли число чётным
+Console.WriteLine ("Введите число, чтобы определить его чётность: ");
+int a = int.Parse (Console.ReadLine()!);
+
+if ( a%2 == 0)
+{
+    Console.WriteLine ("Число чётное. ");
+}
+else
+{
+    Console.WriteLine ("Число нечётное. ");
+}
