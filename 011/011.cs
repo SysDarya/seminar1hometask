0f34171d969936d8a7e448c70244c3ca6934c8ee
Sys Darya <sysdaryaandreevna@gmail.com>
2022-08-08 14:41:27 +0300

    011 task done

---
+++ 011/011.cs
@@ -0,0 +1,13 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine ("Введите любое число: ");
+int a = int.Parse (Console.ReadLine()!);
+
+if (a < 100)
+{
+    Console.WriteLine ("В числе нет третьей цифры.");
+}
+else
+{
+    string str = a.ToString();
+    Console.WriteLine ("Третья цифра числа равна " + str[2] + ".");
+}
