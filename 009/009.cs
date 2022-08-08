ff16edd8455f19c6792a0800d3e2f5b8847abc43
Sys Darya <sysdaryaandreevna@gmail.com>
2022-08-08 14:08:11 +0300

    009 task done

---
+++ 009/009.cs
@@ -0,0 +1,10 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.WriteLine ("Введите трёхзначное число: ");
+int a = int.Parse (Console.ReadLine()!);
+
+int last = a%10;
+int middle = (a/10)%10;
+int first = (a/10)/10;
+
+Console.Write (first);
+Console.WriteLine (last);
