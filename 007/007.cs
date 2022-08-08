436c17aa9d498161b57f5183f1469264c3572dd0
Sys Darya <sysdaryaandreevna@gmail.com>
2022-08-08 13:27:07 +0300

    007 task done

---
+++ 007/007.cs
@@ -0,0 +1,4 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.WriteLine ("Введите трёхзначное число: ");
+int a = int.Parse (Console.ReadLine()!);
+Console.WriteLine ("Вторая цифра числа: " + (a/10)%10);
