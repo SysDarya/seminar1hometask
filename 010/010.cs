6f604c34faf7cd8931ab2ad2a6d389f6d018c18a
Sys Darya <sysdaryaandreevna@gmail.com>
2022-08-08 14:24:44 +0300

    010 task done

---
+++ 010/010.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine ("Введите любое число: ");
+int a = int.Parse (Console.ReadLine()!);
+Console.WriteLine ("Введите делитель, чтобы проверить кратность первого числа делителю: ");
+int b = int.Parse (Console.ReadLine()!);
+
+if ( a%b == 0 )
+{
+    Console.WriteLine ("Число " + a + " кратно числу " + b + ".");
+}
+else
+{
+    Console.WriteLine ("Число " + a + " не кратно числу " + b + ".");
+    Console.WriteLine ("Остаток от деления равен " + a%b + ".");
+}
