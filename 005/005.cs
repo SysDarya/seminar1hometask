fce9a3ba2400942a21f10f608b6b83f131859f0a
Sys Darya <sysdaryaandreevna@gmail.com>
2022-08-08 13:11:30 +0300

    005 task done

---
+++ 005/005.cs
@@ -0,0 +1,10 @@
+﻿// Показать числа от -N до N
+Console.WriteLine ("Введите число-границу: ");
+int n = int.Parse (Console.ReadLine()!);
+int min = -n;
+
+while ( min <= n )
+{
+    Console.Write (min + ";" + " ");
+    min++;
+}
